2f3eae02c4465a3e742513d32b7e3c999d3567d0
changwoo <ccw@changwooui-MacBookPro.local>
2021-12-31 00:26:19 +0900

    [React] mborder frame & mate

--- for_bot/src/page/Home.js
+++ for_bot/src/page/Home.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 function Championimages(props){
   return(
     <>
-    <img alt ={props.champ['id']} src = {props.champ['url']} height='50' width='50'></img>
+      <img alt ={props.champ['id']} src = {props.champ['url']} height='50' width='50'></img>
     </>
   );
 }
@@ -57,48 +57,4 @@ function Home (){
     )
 }
 
-
-const champion_images = [
-  {
-    id:'Aatrox.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Aatrox.png'
-  },
-  {
-    id:'Ahri.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Ahri.png'
-  },
-  {
-    id:'Akali.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Akali.png'
-  },
-  {
-    id:'Alistar.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Alistar.png'
-  },
-  {
-    id:'Amumu.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Amumu.png'
-  },
-  {
-    id:'Anivia.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Anivia.png'
-  },
-  {
-    id:'Annie.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Annie.png'
-  },
-  {
-    id:'Ashe.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Ashe.png'
-  },
-  {
-    id:'AurelionSol.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/AurelionSol.png'
-  },
-  {
-    id:'Azir.png',
-    url:'http://14.32.21.158:8081/forbot/v1/champion/images/Azir.png'
-  }
-];
-  
-  export default Home;
+export default Home;
--- for_bot/src/page/Mate.js
+++ for_bot/src/page/Mate.js
@@ -1,23 +1,67 @@
 import React, { Component } from "react";
+import {useState}from 'react'
+import {useEffect} from 'react';
+import './Mate.css';
+import axios from 'axios';
 
 
-class Mate extends React.Component{
-    constructor(props) {
-      super();
-  
-      this.state = {
-        menu: 3,
-      };
-    }
 
-    render(){
-        return(
-            <>
-            <div>Mate</div>
-            </>
-        )
-    }
-  }
+function imageClick(){
+  console.log("click");
+}
+function Championimages(props){
+  return(
+    <>
+      <img alt ={props.champ['id']} src = {props.champ['url']} height='50' width='50' onClick={()=>imageClick()}></img>
+    </>
+  );
+}
+
+function Mate (){
+
+  let [champions, change_champions] = useState([]);
+  const [searchField, setSearchfield] = useState("");
+  let [filterImages, setFilterimages] = useState([]);
   
-  
-  export default Mate;
+  useEffect(()=>{
+    const apiCall = async () => {
+      await axios.get(`http://${global.serverAdress}/forbot/v1/champion/img/all-url`)
+      .then(res => change_champions(res.data.champion_images))
+      .catch(error => console.log(error))
+    };
+    apiCall();
+  },[])
+
+  useEffect(() => {
+    setFilterimages(()=>
+      champions.filter((c) => c.id.toLowerCase().includes(searchField.toLowerCase()))
+    );
+    
+    
+  }, [searchField, champions]);
+
+  return(
+      <>
+        <div>Mate</div>
+        <div>검색
+          <input type ="search" placeholder="searchField" onChange={(e) => setSearchfield(e.target.value)}></input>
+        </div>
+
+        <div className = "mate_rows">
+          {
+            
+            filterImages.map(function(n, i){
+              return(
+                <Championimages champ = {filterImages[i]} key={filterImages[i]['id']}/>
+              )
+            })
+          }
+        </div>
+
+        <div> ?? 클릭 이벤트 후 여기에 챔피언 보여주기</div>
+        
+      </>
+    )
+}
+
+export default Mate;

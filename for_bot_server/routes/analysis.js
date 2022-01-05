8e4b0a2792a91f67584caf4ec86999e1efc65ae6
howard1005 <tom558@naver.com>
2022-01-05 23:22:25 +0900

    [SERVER] update bottom-match

--- for_bot_server/routes/analysis.js
+++ for_bot_server/routes/analysis.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const pythonHandler = require('../python_handler/python_handler')
 
-router.get(`/bottom-match/:user_name`, function(req, res){
-    res.send("TODO");
+router.get(`/bottom-match`, function(req, res){
+    pythonHandler(__dirname + '/../../riot_api/riot_dispatcher.py', 
+                    ['riot-api/get-summoner-by-summoner-name', 'hide on bush'],
+                    (data) => res.send({'data' : data}));
 });
 
 

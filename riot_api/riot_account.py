bbe2512113362bee91be1e7735dd4e99488435e5
howard1005 <tom558@naver.com>
2022-01-15 23:48:19 +0900

    feat : implement account api

--- riot_api/riot_account.py
+++ riot_api/riot_account.py
@@ -1,7 +1,16 @@
 from riot_api_config import RiotApiConfig
+from riot_summoner import RiotSummoner
 
 class RiotAccount:
-    def __init__(self) -> None:
+    def __init__(self, riot_summoner: RiotSummoner) -> None:
+        self.riot_summoner = riot_summoner
+
         self.__url = RiotApiConfig().url
         self.__get = RiotApiConfig().get
 
+    def get_account(self):
+        summoner = self.riot_summoner.get_summoner()
+        return self.__get(self.__url('asia', "/riot/account/v1/accounts/by-puuid/{}".format(summoner['puuid']))).json()
+
+    def get_account_of_me(self):
+        return self.__get(self.__url('asia', "/riot/account/v1/accounts/me")).json()

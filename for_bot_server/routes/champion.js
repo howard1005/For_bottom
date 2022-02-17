f951583d9ec9d658429b4e0abea80c0fc0116d83
howard1005 <tom558@naver.com>
2022-02-17 23:42:59 +0900

    [SERVER] feat : set db

--- for_bot_server/routes/champion.js
+++ for_bot_server/routes/champion.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const db = require('../models/index');
 
 // router.use(`/images`, function(req, res, next) {
 //     console.log(`call static images ${req.url}`);

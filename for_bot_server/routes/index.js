0651049df1e2153bac1bcc0b4ea55cf9ed489a29
howard1005 <tom558@naver.com>
2022-02-19 23:59:55 +0900

    [SERVER] fix : static path set

--- for_bot_server/routes/index.js
+++ for_bot_server/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get(`/`, function(req, res){
     console.log("[index][/] " + Date());
-    release_path = path.join(__dirname, "../../for_bot/build/index.html")
+    release_path = path.join(global.resource_dir, "/index.html")
     console.log("[index][/] " + "release_path : " +  release_path);
     fs.exists(release_path, exists => {
         if(exists) res.sendFile(release_path);
--- for_bot_server/server.js
+++ for_bot_server/server.js
@@ -4,6 +4,7 @@ const recommendRouter = require('./routes/recommend');
 const analysisRouter = require('./routes/analysis');
 
 const sequelize = require('./models').sequelize;
+const path = require("path");
 
 const express = require("express");
 const app = express();
@@ -13,6 +14,7 @@ sequelize.sync();
 global.logger = require('./config/winston');
 global.serverAdress = "211.218.215.226:8081";
 global.port = 8080;
+global.resource_dir = path.join(__dirname, "../for_bot/build")
 
 app.listen(port, function(){
     console.log(`for bot server start ${global.port} \n external server adress : ${global.serverAdress}`);
@@ -26,6 +28,7 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use('/static', express.static(path.join(global.resource_dir, "/static")));
 
 app.use('/', indexRouter);
 app.use('/forbot/v1/champion', championRouter);

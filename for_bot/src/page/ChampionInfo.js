54ae242d16e90bdb41573f264d1397df7d304342
changwoo <ccw@changwooui-MacBookPro.local>
2022-02-20 22:37:06 +0900

    [React] update cards

--- for_bot/src/page/ChampionInfo.js
+++ for_bot/src/page/ChampionInfo.js
@@ -11,11 +11,7 @@ function ChampionInfo (){
         <>
             <section className={styles.info_section}>
                 <img className={styles.champion_img} src="http://www.simplegame.co.kr/data/editor/1601/4980aecb2de311df98ce93789a9018a2_1453270224_21.PNG"></img>
-                <div className={styles.champion_info}>
-                    <span className ={styles.champion_name}>champion name</span>
-                    <div className={styles.champion_skills}>champion skills</div>
-                    
-                </div>
+
                 <BasicCard></BasicCard>
             </section>
         </>

091f102f1c332dadf216d83a1eb867c79b305aa4
howard1005 <tom558@naver.com>
2022-03-03 01:08:21 +0900

    [SERVER] feat : spell res api

---
+++ for_bot_server/routes/spell.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const db = require('../models/index');
+
+router.get(`/img/all-url`, function(req, res){
+    global.logger.info(`[${__filename}][/img/all-url] `, req.headers);
+    (new Promise((resolve, reject) => {
+        imgs_path = path.join(global.dragontail_path, `/img/spell`)
+        fs.readdir(imgs_path, function(error, filelist){
+            var imgUrlList = [];
+            for (var i in filelist) {
+                var fileName = filelist[i]
+                imgUrlList.push({id : fileName.replace('.png',''), url : `http://${global.serverAdress}/dragontail/img/spell/${fileName}`});
+            }
+            resolve(imgUrlList);
+        })
+    })).then((imgUrlList) => res.send({champion_images : imgUrlList}))
+});
+
+module.exports = router;

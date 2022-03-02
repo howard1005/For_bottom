ffecc8885574446e8fad282069a39a1d4040e8b1
changwoo <ccw@changwooui-MacBookPro.local>
2022-03-02 23:44:03 +0900

    [React] add list listitem

--- for_bot/src/page/ChampionInfo.js
+++ for_bot/src/page/ChampionInfo.js
@@ -6,13 +6,47 @@ import axios from 'axios';
 import { motion } from "framer-motion";
 import BasicTabs from './Tabs.js'
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+
 
 function ChampionInfo (){
     return (
         <>
             <section className={styles.info_section}>
                 <img className={styles.champion_img} src="http://www.simplegame.co.kr/data/editor/1601/4980aecb2de311df98ce93789a9018a2_1453270224_21.PNG"></img>
-
+                <List sx={{ width: '50%', maxWidth: 300, bgcolor: 'background.paper' }}>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <ImageIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <WorkIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Work" secondary="Jan 7, 2014" />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <BeachAccessIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Vacation" secondary="July 20, 2014" />
+                </ListItem>
+                </List>
             </section>
             <section>
             <BasicTabs></BasicTabs>

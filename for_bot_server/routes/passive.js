796648e40085664026d0dfef5cd715999555ef96
howard1005 <tom558@naver.com>
2022-03-07 23:36:18 +0900

    [SERVER] feat : passive swagger

--- for_bot_server/routes/passive.js
+++ for_bot_server/routes/passive.js
@@ -4,6 +4,23 @@ const fs = require('fs');
 const path = require('path');
 const db = require('../models/index');
 
+
+ /**
+  * @swagger
+  * /forbot/v1/passive/img/all-url:
+  *   get:
+  *     summary: passive 이미지 모든 url 가져오기
+  *     tags: [Item]
+  *     responses:
+  *       200:
+  *         description: 성공
+  *       403:
+  *         description: BadRequest
+  *       404:
+  *         description: NotFound
+  *       500:
+  *         description: InternalError
+  */
 router.get(`/img/all-url`, function(req, res){
     global.logger.info(`[${__filename}][/img/all-url] `, req.headers);
     (new Promise((resolve, reject) => {

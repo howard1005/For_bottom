8b08d39b30ab43ae13121e9854d49d2dc202926a
changwoo <ccw@changwooui-MacBookPro.local>
2022-03-13 22:26:21 +0900

    [React] add image url

--- for_bot/src/page/Table.js
+++ for_bot/src/page/Table.js
@@ -7,8 +7,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, calories) {
+  return { name, calories };
 }
 
 const rows = [

2c3ca7fece28127717414bf9fbe85e5bf3e5c4fa
howard1005 <tom558@naver.com>
2022-03-17 23:33:43 +0900

    [SERVER] refact : spell api refactoring

--- for_bot_server/routes/spell.js
+++ for_bot_server/routes/spell.js
@@ -8,6 +8,40 @@ const db = require('../models/index');
   * @swagger
   * /forbot/v1/spell/img/all-url:
   *   get:
+  *     summary: spell 모든 url 가져오기
+  *     tags: [Spell]
+  *     responses:
+  *       200:
+  *         description: 성공
+  *       403:
+  *         description: BadRequest
+  *       404:
+  *         description: NotFound
+  *       500:
+  *         description: InternalError
+  */
+  router.get(`/all-url`, function(req, res){
+    global.logger.info(`[${__filename}][/all-url] `, req.headers);
+    (new Promise((resolve, reject) => {
+        imgs_path = path.join(global.dragontail_path, `/img/spell`)
+        fs.readdir(imgs_path, function(error, filelist){
+            var imgUrlList = [];
+            for (var i in filelist) {
+                var fileName = filelist[i]
+                imgUrlList.push({
+                    id : fileName.replace('.png',''), 
+                    img_url : `http://${global.serverAdress}/dragontail/img/spell/${fileName}`});
+            }
+            resolve(imgUrlList);
+        })
+    })).then((imgUrlList) => res.send({spell_images : imgUrlList}))
+});
+
+
+ /**
+  * @swagger
+  * /forbot/v1/spell/img/all-url:
+  *   get:
   *     summary: spell 이미지 모든 url 가져오기
   *     tags: [Spell]
   *     responses:

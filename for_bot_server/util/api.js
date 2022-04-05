6cd4e9a854f44d207b6e61e51cc7adcc43cdbf09
howard1005 <tom558@naver.com>
2022-04-05 22:55:06 +0900

    [SERVER] feat post api

--- for_bot_server/util/api.js
+++ for_bot_server/util/api.js
@@ -3,8 +3,13 @@ let get = function(url){
     return request({url: url, method: "GET"})
 }
 
-let post = function(url){
-    return request({url: url, method: "POST"})
+let post = function(url, body, cb){ // cb(err,httpResponse,body)
+    const options = {
+        uri:url, 
+        method: 'POST',
+        body: body
+    }
+    request.post(options, cb)
 }
 
 module.exports = get;

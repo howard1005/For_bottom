cb09fe036a74b6068e130baa7375097e9c75ddf3
howard1005 <tom558@naver.com>
2022-05-14 23:00:41 +0900

    [RIOT-API] feat : riot api get_matchs

--- riot_api/server/rest_api/urls.py
+++ riot_api/server/rest_api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('/forbot/v1/summoner/<str:name>', riot.get_summoner, name='get_summoner'),
     path('/forbot/v1/league_entries/<str:name>', riot.get_league_entries, name='get_league_entries'),
     path('/forbot/v1/all_league_entries/<str:tier>/<str:division>', riot.get_all_league_entries, name='get_all_league_entries'),
+    path('/forbot/v1/get_matchs/<str:name>', riot.get_matchs, name='get_matchs'),
 ]

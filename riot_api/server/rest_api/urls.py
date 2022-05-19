b838c782e61d657c8c7ef788cd6a1934474b4ada
howard1005 <tom558@naver.com>
2022-05-19 23:38:06 +0900

    [RIOT-API] feat : api add get_match_timelines

--- riot_api/server/rest_api/urls.py
+++ riot_api/server/rest_api/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('forbot/v1/league_entries/<str:name>', riot.get_league_entries, name='get_league_entries'),
     path('forbot/v1/all_league_entries/<str:tier>/<str:division>', riot.get_all_league_entries, name='get_all_league_entries'),
     path('forbot/v1/get_matchs/<str:name>', riot.get_matchs, name='get_matchs'),
+    path('forbot/v1/get_match_timelines/<str:name>', riot.get_match_timelines, name='get_match_timelines'),
+    
 ]

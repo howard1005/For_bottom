12cbf017041b57c56343b5a7aea2f0088cfed074
howard1005 <tom558@naver.com>
2022-05-25 23:53:51 +0900

    [RIOT-API] feat : api add url get_all_champion_mastery_entries

--- riot_api/server/rest_api/urls.py
+++ riot_api/server/rest_api/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('forbot/v1/all_league_entries/<str:tier>/<str:division>', riot.get_all_league_entries, name='get_all_league_entries'),
     path('forbot/v1/get_matchs/<str:name>', riot.get_matchs, name='get_matchs'),
     path('forbot/v1/get_match_timelines/<str:name>', riot.get_match_timelines, name='get_match_timelines'),
-    path('forbot/v1/get_match_timelines/<str:name>', riot.get_all_champion_mastery_entries, name='get_all_champion_mastery_entries'),
+    path('forbot/v1/get_all_champion_mastery_entries/<str:name>', riot.get_all_champion_mastery_entries, name='get_all_champion_mastery_entries'),
     
 ]

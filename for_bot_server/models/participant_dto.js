6dabf999f71c746e4ecfecc300db46d0c233746f
howard1005 <tom558@naver.com>
2022-06-20 21:39:27 +0900

    [SERVER] feat : add field of info dto continue

--- for_bot_server/models/participant_dto.js
+++ for_bot_server/models/participant_dto.js
@@ -7,6 +7,11 @@ module.exports = (sequelize, DataTypes) => {
         baronKills: { type: DataTypes.INTEGER, allowNull: false },
         bountyLevel: { type: DataTypes.INTEGER, allowNull: false },
         champExperience: { type: DataTypes.INTEGER, allowNull: false },
+        champLevel: { type: DataTypes.INTEGER, allowNull: false },
+        championId: { type: DataTypes.INTEGER, allowNull: false },
+        championName: { type: DataTypes.STRING, allowNull: false },
+        championTransform: { type: DataTypes.INTEGER, allowNull: false },
+
         
       },
       { timestamps: false }

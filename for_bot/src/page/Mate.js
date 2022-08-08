5774b1a61ab1b2cfc7daf3132e1997f095afe344
changwoo <ccw@changwooui-MacBookPro.local>
2022-08-08 23:54:26 +0900

    update mate.js

--- for_bot/src/page/Mate.js
+++ for_bot/src/page/Mate.js
@@ -247,35 +247,37 @@ function Mate (){
          
           <Paper elevation={3} sx={{width: 400, height: 500}} >
             <div className="">
-            <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell aligh="left">순위</TableCell>
+                      <TableCell align="">챔피언</TableCell>
+                      <TableCell align="">티어</TableCell>
+                      <TableCell align="">승률</TableCell>
+                      <TableCell align="">픽률</TableCell>
+                      <TableCell align="">벤률</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow
+                        key={row.name}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell component="th" scope="row">
+                          {row.name}
+                        </TableCell>
+                        <TableCell align="">{row.calories}</TableCell>
+                        <TableCell align="">{row.fat}</TableCell>
+                        <TableCell align="">{row.carbs}</TableCell>
+                        <TableCell align="">{row.protein}</TableCell>
+                        <TableCell align="">{row.protein}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </div>
           </Paper>
 
